be707f40523ab92e36845d92b10b35c161253d2f
Israel Pérez Saucedo <neomatrixisra25@hotmail.com>
2017-06-15 22:15:28 -0500

    Subiendo RetoN :D

---
+++ Start/RetoN/Services/ServiceHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RetoN.Services
+{
+    public class ServiceHelper
+    {
+        MobileServiceClient clienteServicio = new MobileServiceClient(@"http://xamarinchampions.azurewebsites.net");
+
+        private IMobileServiceTable<TorneoItem> _TorneoItemTable;
+
+        public async Task InsertarEntidad(string direccionCorreo, string reto, string AndroidId)
+        {
+            _TorneoItemTable = clienteServicio.GetTable<TorneoItem>();
+
+
+            await _TorneoItemTable.InsertAsync(new TorneoItem
+            {
+                Email = direccionCorreo,
+                Reto = reto,
+                DeviceId = AndroidId
+            });
+        }
+
+        public async Task<List<TorneoItem>> BuscarRegistros(string correo)
+        {
+            _TorneoItemTable = clienteServicio.GetTable<TorneoItem>();
+            List<TorneoItem> items = await _TorneoItemTable.Where(
+                    torneoItem => torneoItem.Email == correo
+                ).ToListAsync();
+
+            return items;
+        }
+    }
+}
